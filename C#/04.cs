0e8e85a091b8ae888bde8621af104b3178fb4fc5
Lucas Velez <lucasvelez2013@gmail.com>
2025-10-15 12:39:04 -0300

    04 cs
    Implement a class to check for prime numbers and identify primes between 1 and 100.

---
+++ C#/04.cs
@@ -0,0 +1,19 @@
+/*
+ * Escribe un programa que se encargue de comprobar si un número es o no primo.
+ * Hecho esto, imprime los números primos entre 1 y 100.
+ */
+internal class NumerosPrimos
+{
+    public bool esPrimo(int numero)
+    {
+        if (numero < 2) return false;
+        for (int i = 2; i <= Math.Sqrt(numero); i++)
+        {
+            if (numero % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
