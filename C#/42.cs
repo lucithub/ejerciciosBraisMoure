f2667e28e8de269228eff0a2e6d8fd119a926478
Lucas Velez <lucasvelez2013@gmail.com>
2026-02-19 11:16:08 -0300

    42 cs
    Implement a method to draw Pascal's Triangle based on the specified size.

---
+++ C#/42.cs
@@ -0,0 +1,31 @@
+/*
+ * Crea una función que sea capaz de dibujar el "Triángulo de Pascal"
+ * indicándole únicamente el tamaño del lado.
+ *
+ * - Aquí puedes ver rápidamente cómo se calcula el triángulo:
+ *   https://commons.wikimedia.org/wiki/File:PascalTriangleAnimated2.gif
+ */
+public class TrianguloDePascal
+{
+    public void DrawPascalTriangle(int size)
+    {
+        int[,] triangulo = new int[size, size];
+        for (int i = 0; i < size; i++)
+        {
+            for (int k = 0; k < size - i; k++)
+            {
+                Console.Write(" ");
+            }
+            for (int j = 0; j <= i; j++)
+            {
+                if (j == 0 || j == i) triangulo[i, j] = 1;
+                else
+                {
+                    triangulo[i, j] = triangulo[i - 1, j - 1] + triangulo[i - 1, j];
+                }
+                Console.Write(triangulo[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
